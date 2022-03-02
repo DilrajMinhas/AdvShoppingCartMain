b2556149928139c1b7160fa4934b9896b440e5bf
Dilraj <dilraj.minhas1@gmail.com>
2022-03-02 00:16:55 -0800

    Uploading New User Register

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -1,1 +1,18 @@
+from faker import Faker
+fake = Faker(locale='en_CA')
 adshopcart_url = 'https://advantageonlineshopping.com/#/'
+advantage_shop_cart_url= 'https://advantageonlineshopping.com/#/register'
+new_username = fake.user_name()
+email = fake.email()
+new_password = fake.password()
+first_name = fake.first_name()
+last_name = fake.last_name()
+phone = fake.phone_number()
+country =fake.country()
+city = fake.city()
+province=fake.postalcode()
+address = fake.address().replace("\n", "")
+
+
+
+
--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -1,8 +1,10 @@
 import datetime
 from time import sleep
 from selenium import webdriver
-import adshopcart_locators as locators
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+import adshopcart_locators as locators
 
 driver= webdriver.Chrome(r'C:\Users\hsmdilraj\PycharmProject\moodle\chromedriver.exe')
 
@@ -17,15 +19,41 @@ def setUp():
     else:
         print(f' We are not at webpage')
         sleep(0.25)
-
-
 def tearDown():
     if driver is not None:
-        print (f'------------------')
+        print(f'------------------')
         print(f'Test completed at:{datetime.datetime.now()}')
         sleep(5)
         driver.close()
         driver.quit()
+def create_new_user():
+    if driver.current_url==locators.adshopcart_url:
+       driver.find_element(By.ID,"menuUser").click()
+       sleep(5)
+       assert driver.find_element(By.LINK_TEXT, 'CREATE NEW ACCOUNT').is_displayed()
+       sleep(5)
+       driver.find_element(By.LINK_TEXT,'CREATE NEW ACCOUNT').click()
+       sleep(5)
+       driver.find_element(By.CSS_SELECTOR,"[name='usernameRegisterPage']").send_keys(locators.new_username)
+       sleep(5)
+    # Click by the email,password open field and enter fake password
+       driver.find_element(By.CSS_SELECTOR,"[name='emailRegisterPage']").send_keys(locators.email)
+       driver.find_element(By.CSS_SELECTOR, "[name='passwordRegisterPage']").send_keys(locators.new_password)
+       driver.find_element(By.CSS_SELECTOR,"[name='confirm_passwordRegisterPage']").send_keys(locators.new_password)
+# Enter fake data into username open field
+    driver.find_element(By.CSS_SELECTOR,"[name='first_nameRegisterPage']").send_keys(locators.first_name)
+    driver.find_element(By.CSS_SELECTOR, "[name='last_nameRegisterPage']").send_keys(locators.last_name)
+    driver.find_element(By.CSS_SELECTOR,"[name='addressRegisterPage']").send_keys(locators.address)
+    driver.find_element(By.CSS_SELECTOR,"[name='postal_codeRegisterPage']").send_keys(locators.province)
+    driver.find_element(By.CSS_SELECTOR,"[name='i_agree']").click()
+    driver.find_element(By.ID,'register_btnundefined').click()
+    print(f'user is registered')
 
-#setUp()
-#tearDown()
+
+setUp()
+create_new_user()
+tearDown()
+
+
+
+#create_new_user()
